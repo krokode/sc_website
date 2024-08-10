60ee9f340d44a60cb30774d9de556ee913682d47
Alek Judaev <alekjudaev@Maxims-Mac-mini.local>
2024-08-10 23:02:55 +0200

    ip access_route[-1]

--- sc_app2.py
+++ sc_app2.py
@@ -47,10 +47,11 @@ def get_location(ip):
 def index():
     # Render the HTML template
     # user_ip = request.remote_addr  # Get the user's IP address
-    # user_ip = request.access_route[-1]
     # user_ip = request.headers['X-Forwarded-For']
-    headers_list = request.headers.getlist("X-Forwarded-For")
-    user_ip = headers_list[0] if headers_list else request.remote_addr
+    """  headers_list = request.headers.getlist("X-Forwarded-For")
+    user_ip = headers_list[0] if headers_list else request.remote_addr """
+
+    user_ip = request.access_route[-1]
     location_info = get_location(user_ip)  # Get the location information
     return render_template('index2.html', location=location_info)
 
