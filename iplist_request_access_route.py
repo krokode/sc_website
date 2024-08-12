3b051ae9fc49dde1bfd33561e2a51dffde3f4bb8
Alek Judaev <alekjudaev@Maxims-Mac-mini.local>
2024-08-12 16:59:54 +0200

    index0.html for location[i] ul

--- iplist_request_access_route.py
+++ iplist_request_access_route.py
@@ -1,19 +1,42 @@
+import json
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
+
+
+def get_ip_based_geolocation(ip):
+    response = requests.get(f'https://ipinfo.io/{ip}/json')
+    data = response.json()
+    print(data)
+
+    return {
+        "ip": data.get("ip"),
+        "city": data.get("city"),
+        "region": data.get("region"),
+        "country": data.get("country"),
+        "loc": data.get("loc"),  # Latitude and Longitude in "lat,lon" format
+        "organization": data.get("org"),
+        "postal": data.get("postal"),
+        "timezone": data.get("timezone"),
+        "privacy": data.get("privacy")
+    }
 
 
 @ app.route('/')
 def HomePage():
     # Render the HTML template
-    user_ip = request.remote_addr  # Get the user's IP address
-    print(f"request.remote_addr - {user_ip}")
-    user_ip1 = request.access_route[-1]
-    print(f"request.access_route[-1] - {user_ip1}")
-    user_ip2 = request.access_route
+    """  user_ip1 = request.remote_addr  # Get the user's IP address
+    print(f"request.remote_addr - {user_ip1}") 
+    """
+    """ user_ip2 = request.access_route
     print(f"request.access_route - {user_ip2}")
-
-    return render_template('index0.html')
+    """
+    location_info = {}
+    for i in range(len(request.access_route)):
+        location_info[i] = get_ip_based_geolocation(request.access_route[i])
+        i += 1
+    return render_template('index0.html', location=location_info)
 
 
 if __name__ == "__main__":
